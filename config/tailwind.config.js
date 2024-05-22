1ce63fb286aec2b1c5123e6373cf574ef09a5ac2
VILLAMORGO <villamorgo@gmail.com>
2024-05-23 00:05:23 +0800

    add responsiveness to ui

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -13,6 +13,12 @@ module.exports = {
         sans: ['Inter var', ...defaultTheme.fontFamily.sans],
       },
     },
+    screens: {
+      'sm': '640px',
+      'md': '768px',
+      'lg': '1024px',
+      'xl': '1280px',
+    },
   },
   plugins: [
     require('@tailwindcss/forms'),
