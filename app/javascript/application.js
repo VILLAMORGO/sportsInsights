6caf07379fd626e3b73602d2a782fb492561c11d
VILLAMORGO <villamorgo@gmail.com>
2024-06-01 19:16:37 +0800

    fix naming issue in script import

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,6 +1,6 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
-import './custom/jquery'
-import "./custom/chat"
-import "./custom/autoScroll"
+import './custom/jquery.js'
+import "./custom/chat.js"
+import "./custom/autoScroll.js"
