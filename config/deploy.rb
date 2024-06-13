f3451a4686f593d0b7f84782915f0b871106a5ad
VILLAMORGO <villamorgo@gmail.com>
2024-06-13 18:47:29 +0800

    comment out checking for revisions

--- config/deploy.rb
+++ config/deploy.rb
@@ -60,17 +60,17 @@ end
 
 namespace :deploy do
   desc "Make sure local git is in sync with remote."
-  task :check_revision do
-    on roles(:app) do
+#   task :check_revision do
+#     on roles(:app) do
 
-      # Update this to your branch name: master, main, etc. Here it's main
-      unless `git rev-parse HEAD` == `git rev-parse origin/master`
-        puts "WARNING: HEAD is not the same as origin/master"
-        puts "Run `git push` to sync changes."
-        exit
-      end
-    end
-  end
+#       # Update this to your branch name: master, main, etc. Here it's main
+#       unless `git rev-parse HEAD` == `git rev-parse origin/master`
+#         puts "WARNING: HEAD is not the same as origin/master"
+#         puts "Run `git push` to sync changes."
+#         exit
+#       end
+#     end
+#   end
 
   desc 'Initial Deploy'
   task :initial do
